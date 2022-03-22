3eddebec8da1847e6a87b3c32445e84b611bc080
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-03-22 20:03:58 +0200

    Create UserViewModel and Fetch User Data form API

--- mvvm_RxSwift/mvvm_RxSwift/ViewController.swift
+++ mvvm_RxSwift/mvvm_RxSwift/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var viewModel = UserViewModel()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ mvvm_RxSwift/mvvm_RxSwift/ViewModel/UserViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  UserViewModel.swift
+//  mvvm_RxSwift
+//
+//  Created by Eslam Ali  on 22/03/2022.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class UserViewModel {
+
+    var users = BehaviorSubject(value: [User]())
+    
+    func fetchUsers()  {
+        let url = " https://jsonplaceholder.typicode.com/posts"
+        guard let urlString = URL(string: url) else  {return}
+        
+        let task = URLSession.shared.dataTask(with: urlString) {data , _ , error in
+            guard let data = data else {return}
+            do {
+                let responseData = try JSONDecoder().decode([User].self, from: data)
+                self.users.on(.next(responseData))
+            }catch {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+        
+        
+    }
+    
+    
+    
+}
