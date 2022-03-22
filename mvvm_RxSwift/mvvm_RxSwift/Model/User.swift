ebe411cff38a12b0b89ea1fbcca3e75c5294bd6e
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-03-22 20:03:30 +0200

    Create User Model

---
+++ mvvm_RxSwift/mvvm_RxSwift/Model/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  mvvm_RxSwift
+//
+//  Created by Eslam Ali  on 22/03/2022.
+//
+
+import Foundation
+
+
+struct User : Codable {
+    let id : Int
+    let userId : Int
+    let title : String
+    let body  : String
+    
+}
