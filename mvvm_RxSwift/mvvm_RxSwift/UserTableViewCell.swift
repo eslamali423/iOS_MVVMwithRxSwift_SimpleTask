c7b7b93e3095eaaefed00128ac0de3835dbe63c2
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-03-22 20:27:18 +0200

    Binding Tableview and present user data

---
+++ mvvm_RxSwift/mvvm_RxSwift/UserTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UserTableViewCell.swift
+//  mvvm_RxSwift
+//
+//  Created by Eslam Ali  on 22/03/2022.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- mvvm_RxSwift/mvvm_RxSwift/ViewController.swift
+++ mvvm_RxSwift/mvvm_RxSwift/ViewController.swift
@@ -6,17 +6,56 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
     var viewModel = UserViewModel()
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    var bag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+       // tableView.delegate = self
+     //   tableView.dataSource =  self
+
+        viewModel.fetchUsers()
+        bindTableView()
+        
+    }
+    
+    func  bindTableView() {
+        viewModel.users.bind(to: tableView.rx.items(cellIdentifier: "cell", cellType: UITableViewCell.self)) { row , userItem , cell in
+            cell.textLabel?.text = userItem.title
+            cell.detailTextLabel?.text = "\(userItem.id)"
+            
+        }.disposed(by: bag)
+        tableView.rx.setDelegate(self).disposed(by: bag)
+        tableView.rx.itemSelected.subscribe(onNext: { indexPath in
+            print(indexPath.row)
+        }).disposed(by: bag)
+        
+        
     }
 
 
+}
+
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! UserTableViewCell
+       
+        return cell
+    }
+    
+    
 }
 
